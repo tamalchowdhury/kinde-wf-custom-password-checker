e13fe5f46621b7fb5663acaa2a6e83c0b828a3e6
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2025-09-25 23:05:59 +0600

    Test workflow

---
+++ kindeSrc/environment/workflows/customPasswordValication/workflow.ts
@@ -0,0 +1,27 @@
+import {
+  onNewPasswordProvidedEvent,
+  WorkflowSettings,
+  WorkflowTrigger,
+  invalidateFormField
+} from "@kinde/infrastructure";
+
+// The setting for this workflow
+export const workflowSettings: WorkflowSettings = {
+  name: "Custom Password Validation",
+  id: "onNewPasswordProvided",
+  trigger: WorkflowTrigger.NewPasswordProvided,
+  bindings: {
+    "kinde.widget": {}
+  }
+};
+
+// The workflow code to be executed when the event is triggered
+export default async function Workflow(event: onNewPasswordProvidedEvent) {
+  const { firstPassword } = event.context.auth
+
+  if(firstPassword !== "hello")
+
+  invalidateFormField(firstPassword, "The first password is not hello!")
+
+
+}
